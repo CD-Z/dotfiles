5ac59c333a845758b74ae588c4556e0b949a9cbd
Patrick Loser <p_loser_Austria@gmx.at>
2024-02-03 14:53:00 +0100

    fedora fix

--- hypr/scripts/resetxdgportal.sh
+++ hypr/scripts/resetxdgportal.sh
@@ -7,6 +7,6 @@ killall xdg-desktop-portal-lxqt
 killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
 sleep 1
-/usr/lib/xdg-desktop-portal-hyprland &
+/usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+/usr/libexec/xdg-desktop-portal &
